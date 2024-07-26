81a8039980fff247c581fd0176c332e1ce5957ec
BBenyamini <bukombebenjamin@gmail.com>
2024-07-26 22:49:53 +0200

    added new dataframe

--- demo.py
+++ demo.py
@@ -6,48 +6,48 @@ import numpy as np
 import altair as alt
 
 
-# st.write("Hello World, let's read and display data!")
+st.write("Hello World, let's read and display data!")
 
-# #to run a streamlit app in  the browser, use: streamlit run app-name
+#to run a streamlit app in  the browser, use: streamlit run app-name
 
-# st.header('st.button')
-# if st.button('Say hello'):
-#     st.write('Why hello there')
-# else:
-#     st.write('Goodbye')
-
-
-# st.header('st.write')
-# st.write('Hello, *World!* :sunglasses:')
-# st.write(1234)
-
-# df = pd.DataFrame({
-#      'first column': [1, 2, 3, 4],
-#      'second column': [10, 20, 30, 40]
-#      })
-
-# st.write(df)
+st.header('st.button')
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
 
 
-# st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+st.header('st.write')
+st.write('Hello, *World!* :sunglasses:')
+st.write(1234)
 
-# df2 = pd.DataFrame(
-#      np.random.randn(200, 3),
-#      columns=['a', 'b', 'c'])
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+
+st.write(df)
 
 
-# c=alt.Chart(weather).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
-# st.write(c)
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
 
-# st.write('Here is a snipet of the code:')
-# st.code(language='python', line_numbers= True, 
-#         body='''
-# import numpy as np
-# import pandas as pd
-# import xarray as xr
-# import matplotlib.pyplot as plt'''
-# )
+
+c=alt.Chart(df2).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
+
+st.write('Here is a snipet of the code:')
+st.code(language='python', line_numbers= True, 
+        body='''
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt'''
+)
 
 weather=pd.read_csv("/home/bebu0001/mydrive/streamlit_repo/meteo/430_meteo_pcp_trig.csv", sep=";")
 weather.head()
